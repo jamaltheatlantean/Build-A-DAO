e00a27cec70881697b065845a451762cddc6b392
Gabriel Isobara <jamaltheatlantean@gmail.com>
2023-03-21 16:30:00 +0100

    update deploy script

--- deploy/deploy-DAO.js
+++ deploy/deploy-DAO.js
@@ -1,41 +1,17 @@
 const {
-    getNamedAccounts,
-    deployments,
     network,
-    run,
-    ethers,
   } = require("hardhat");
-  const {
-    networkConfig,
-    developmentChains,
-    VERIFICATION_BLOCK_CONFIRMATIONS,
-  } = require("../helper-hardhat-config");
-  
-  const waitBlockConfirmations = developmentChains.includes(network.name)
-  ? 1
-  : VERIFICATION_BLOCK_CONFIRMATIONS;
   
   module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
-    const chainId = network.config.chainId;
   
     const arguments = [];
     const dao = await deploy("DAO", {
       from: deployer,
       args: arguments,
       log: true,
-      waitConfirmations: waitBlockConfirmations || 1,
     });
-  
-    // Verify the deployment
-    if (
-      !developmentChains.includes(network.name) &&
-      process.env.ETHERSCAN_API_KEY
-    ) {
-      log("Verifying...");
-      await verify(dao.address, arguments);
-    }
   
     const networkName = network.name == "hardhat" ? "localhost" : network.name;
     log(`npx hardhat run scripts/deploy-DAO.js --network ${networkName}`);
